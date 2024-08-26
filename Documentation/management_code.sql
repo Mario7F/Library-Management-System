50e12b6f67587cc5ff160aa900c903a5b8da2bec
Mario Fletcher <mariojfletcher@gmail.com>
2024-08-26 07:56:25 -0700

    Update management_code.sql

--- Documentation/management_code.sql
+++ Documentation/management_code.sql
@@ -1,7 +1,10 @@
+-- Create a Database
 CREATE DATABASE LibraryManagement;
 
+-- Select the datbase
 USE LibraryManagement;
 
+-- Create a table for the authors
 create table Authors (
 	author_id INT auto_increment
 PRIMARY KEY,
@@ -10,12 +13,12 @@ PRIMARY KEY,
 		birthday DATE,
 		nationality VARCHAR(100)
 	);
-
+    -- Create table for the Categories
 CREATE TABLE Categories (
 	category_id INT auto_increment PRIMARY KEY,
     category_name VARCHAR(100) NOT NULL
     );
-    
+    -- Create table for the Books
 CREATE TABLE Books (
 		book_id INT auto_increment PRIMARY KEY,
 		title varchar(255) NOT NULL,
@@ -26,7 +29,7 @@ CREATE TABLE Books (
         FOREIGN KEY (category_id)
 	REFERENCES Categories(category_id)
     );
-    
+    -- Create BooksAuthors
     CREATE TABLE BookAuthors (
 		book_id INT,
         author_id INT,
@@ -36,24 +39,24 @@ CREATE TABLE Books (
 		FOREIGN KEY (author_id)
 	REFERENCES Authors(author_id) ON DELETE CASCADE
     );
-    
+    -- Insert into Categories (fiction, i.e)
     INSERT INTO Categories
     (category_name) VALUES ('Fiction'),
     ('NonFiction'), ('Science'), ('History'),
     ('Mathmatics'), ('Politics'), ('Design'),
     ('Health'), ('Mystery'), ('ComputerScience'), ('InformationTechnology'),
     ('Finance'), ('Adventure'), ('Religion');
-    
+    -- Insert into Authors name
     INSERT INTO Authors (first_name, last_name, nationality) VALUES
     ('George', 'Orwell', 'British'),
     ('William', 'Shotts', 'American');
-    
+    -- Insert information about book (isbn, i.e)
 INSERT INTO Books (title, isbn, category_id, publication_year,
 copies_available)
 VALUES
 ('1984', '9780451524935', 1, 1949, 2),
 ('The Linux Command Line', '9781593279523', 11, 2019, 1);
-
+    -- Connect authors to their book
 INSERT INTO BookAuthors (book_id, author_id)
 VALUES
 (1, 1), (2, 2);
